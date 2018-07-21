dcc2eb8eef94e333042372aedd6a39e1756e437b
Louis-He <1726110778@qq.com>
2018-07-21 15:21:21 +0800

    intialize the project

---
+++ pages/index/index.js
@@ -0,0 +1,21 @@
+//index.js
+//获取应用实例
+const app = getApp()
+Page({
+  onLoad() {
+    this.ctx = wx.createCameraContext()
+  },
+  takePhoto() {
+    this.ctx.takePhoto({
+      quality: 'high',
+      success: (res) => {
+        this.setData({
+          src: res.tempImagePath
+        })
+      }
+    })
+  },
+  error(e) {
+    console.log(e.detail)
+  }
+})
